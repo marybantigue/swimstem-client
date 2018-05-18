54cc5632007f3fe69d1927d1d09ffca2749db1e4
Mary <mary.bantigue@datasouth.co.uk>
2018-05-18 09:57:03 +0100

    first commit

---
+++ src/store/reducers/errors.js
@@ -0,0 +1,14 @@
+import { ADD_ERROR, REMOVE_ERROR } from '../actionTypes';
+
+export default ( state = {message: null }, action) => {
+  switch (action.type) {
+    case ADD_ERROR:
+      return { ...state, message: action.error };
+      break;
+    case REMOVE_ERROR:
+      return { ...state, message: null };
+      break;
+    default:
+      return state;
+  }
+}
